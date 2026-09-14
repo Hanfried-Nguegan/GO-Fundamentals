5ceee00f7abda14adb29f1851e58c64320c65bd3
Hanfried Nguegan <nhanfried@gmail.com>
2026-09-14 14:10:03 +0200

    Static Types in Go

---
+++ Static Types/staticTypes.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var firstname string = "Hanfried"
+
+	var lastname string = "Nguegan"
+
+	var fullname string = firstname + " " + lastname
+
+	var age int = 30
+
+	fmt.Println("Hi my name is", fullname, "and I am", age, "years old")
+}
