507f935ffdaad554ad442b7add3aeef6e2b06cae
Hanfried Nguegan <nhanfried@gmail.com>
2026-09-15 00:40:37 +0200

    Getting input from the user

--- Go Essentials/investment_calculator.go
+++ Go Essentials/investment_calculator.go
@@ -6,9 +6,23 @@ import (
 )
 
 func main() {
-	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
+	const inflationrate = 6.5
+	var investmentAmount float64
+	var years float64
+	var expectedReturnRate = 2.5
+
+	fmt.Print("Enter investment amount: ")
+	fmt.Scan(&investmentAmount)
+
+	// fmt.Print("Enter expected return rate: ")
+	// fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Enter number of years: ")
+	fmt.Scan(&years)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationrate/100, years)
 
 	fmt.Println("Future value:", futureValue)
+	fmt.Println("Future Real Value", futureRealValue)
 }
