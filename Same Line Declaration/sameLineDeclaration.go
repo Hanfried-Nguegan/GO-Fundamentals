5dec59fd380436cb5909334ed934cbd1efda6cf7
Hanfried Nguegan <nhanfried@gmail.com>
2026-09-14 14:15:25 +0200

    Same Line Declaration In Go

---
+++ Same Line Declaration/sameLineDeclaration.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	averageOpenRate, displaymessage := .23, "is the average open rate of your message"
+
+	fmt.Println(averageOpenRate, displaymessage)
+}
