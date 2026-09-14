2d06ad8a825eb8b395abbf2144673c2315f9a48d
Hanfried Nguegan <nhanfried@gmail.com>
2026-09-15 00:20:26 +0200

    One line initialisation with short assignment

--- Go Essentials/investment_calculator.go
+++ Go Essentials/investment_calculator.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	var investmentAmount, years, expectedReturnRate float64 = 1000.0, 10.0, 5.5
+	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 
