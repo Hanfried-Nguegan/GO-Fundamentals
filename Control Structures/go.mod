3df1a2fc2b2b639ae6c6226689369e12422ce9e9
Hanfried Nguegan <nhanfried@gmail.com>
2026-09-16 20:45:46 +0200

    If else in Go

---
+++ Control Structures/go.mod
@@ -0,0 +1,3 @@
+module github.com/control_structures
+
+go 1.27.1
---
+++ Control Structures/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	var accountBalance = 1000.0
+
+	fmt.Println("Welcome to Go bank !")
+	fmt.Println("What do you want to do ?")
+	fmt.Println("1. Check balance")
+	fmt.Println("2. Deposit Money")
+	fmt.Println("3. Withdraw money")
+	fmt.Println("4. Exit")
+
+	var choice int
+	fmt.Print("Your choice: ")
+	fmt.Scan(&choice)
+
+	// wantsCheckBalance := choice == 1
+
+	if choice == 1 {
+		fmt.Println("Your balance is: ", accountBalance)
+	} else if choice == 2 {
+		fmt.Print("Your Deposit: ")
+		var depositAmount float64
+		fmt.Scan(&depositAmount)
+		accountBalance += depositAmount
+		fmt.Println("Balance updated ! New Amount:", accountBalance)
+	} else if choice == 3 {
+		fmt.Print("Your Withdrawal: ")
+		var withdrawAmount float64
+		fmt.Scan(&withdrawAmount)
+		accountBalance -= withdrawAmount
+		fmt.Println("You withdrew:", withdrawAmount, "Your new balance is:", accountBalance)
+	} else {
+		fmt.Println("Program Exited")
+	}
+
+	fmt.Println("Your choice is", choice)
+}
