b91af45843ce31c0dffdbee5b1302ef5b8083883
Hanfried Nguegan <nhanfried@gmail.com>
2026-09-19 15:30:02 +0200

    Structs in Go

---
+++ Structs/structs.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type User struct {
+	firstName string
+	lastName  string
+	birthdate string
+	createdAt time.Time
+}
+
+func main() {
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	var appUser User
+
+	appUser = User{
+		firstName: userFirstName,
+		lastName:  userLastName,
+		birthdate: userBirthdate,
+		createdAt: time.Now(),
+	}
+
+	outputPlayerDetails(appUser)
+
+	// ... do something awesome with that gathered data!
+
+	//fmt.Println(firstName, lastName, birthdate)
+}
+
+func outputPlayerDetails(u User) {
+	// how to print out the user data on multiple lines in a single print statement?
+	fmt.Printf("Firstname: %s\n Lastname: %s\n Birthdate: %s\n CreatedAt %s\n", u.firstName, u.lastName, u.birthdate, u.createdAt)
+}
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	var value string
+	fmt.Scan(&value)
+	return value
+}
